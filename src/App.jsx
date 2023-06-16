e49980a217601a1791f9cdd9afc8076ab8ef06b2
komals1402 <ks387406@gmail.com>
2023-06-16 12:33:45 +0530

    moved NEW_GAME state outside of App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import Board from './components/Board';
 import StatusMessage from './components/StatusMessage';
 import History from './components/History';
 import { calculateWinner } from './winner';
+
+const NEW_GAME = [
+  {
+    squares: Array(9).fill(null),
+    isXNext: false,
+  },
+];
+
 function App() {
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null), isXNext: false },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
+
   const [currentMove, setCurrentMove] = useState(0);
 
   const gamingBoard = history[currentMove];
